dc87f4eebf51d3137080eb80906005588e874c5b
张文皓 <zhangwh754@163.com>
2024-12-17 17:21:02 +0800

    feat: antd layout搭建

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,72 @@
-import { useState } from 'react'
+import React from 'react'
+import { LaptopOutlined, NotificationOutlined, UserOutlined } from '@ant-design/icons'
+import type { MenuProps } from 'antd'
+import { Breadcrumb, Layout, Menu, theme } from 'antd'
 
-function App() {
-  const [count, setCount] = useState(0)
+const { Header, Content, Sider } = Layout
+
+const items2: MenuProps['items'] = [UserOutlined, LaptopOutlined, NotificationOutlined].map(
+  (icon, index) => {
+    const key = String(index + 1)
+
+    return {
+      key: `sub${key}`,
+      icon: React.createElement(icon),
+      label: `subnav ${key}`,
+
+      children: new Array(4).fill(null).map((_, j) => {
+        const subKey = index * 4 + j + 1
+        return {
+          key: subKey,
+          label: `option${subKey}`,
+        }
+      }),
+    }
+  }
+)
+
+const App: React.FC = () => {
+  const {
+    token: { colorBgContainer, borderRadiusLG },
+  } = theme.useToken()
 
   return (
-    <>
-      <div>count：{count}</div>
-      <button onClick={() => setCount(count + 1)}>addCount</button>
-    </>
+    <Layout style={{ height: '100vh' }}>
+      <Header style={{ display: 'flex', alignItems: 'center' }}>
+        <div className="demo-logo" />
+      </Header>
+      <Layout>
+        <Sider width={200} style={{ background: colorBgContainer }}>
+          <Menu
+            mode="inline"
+            defaultSelectedKeys={['1']}
+            defaultOpenKeys={['sub1']}
+            style={{ height: '100%', borderRight: 0 }}
+            items={items2}
+          />
+        </Sider>
+        <Layout style={{ padding: '0 24px 24px' }}>
+          <Breadcrumb
+            items={[{ title: 'Home' }, { title: 'List' }, { title: 'App' }]}
+            style={{ margin: '16px 0' }}
+          />
+          <Content
+            style={{
+              flex: 1,
+              overflowY: 'auto',
+              padding: 24,
+              margin: 0,
+              background: colorBgContainer,
+              borderRadius: borderRadiusLG,
+            }}
+          >
+            {Array.from({ length: 1000 }).map((_, index) => (
+              <div>{index}</div>
+            ))}
+          </Content>
+        </Layout>
+      </Layout>
+    </Layout>
   )
 }
 
