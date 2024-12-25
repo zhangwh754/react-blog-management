d02d48d9c224d076355e775c1f6341d6fc41e42a
zhang <zhangwh754@163.com>
2024-12-25 23:46:01 +0800

    feat: 配置Vite服务器，设置主机和端口

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0',
+    port: 14996,
+  },
 })
