6f43aedb9da8b4b2a50daaefe4cd8c5f413656aa
zhang <zhangwh754@163.com>
2024-12-25 20:14:58 +0800

    feat: 添加并使用eslint-plugin-simple-import-sort插件

--- eslint.config.js
+++ eslint.config.js
@@ -4,6 +4,7 @@ import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
 import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended'
+import simpleImportSort from 'eslint-plugin-simple-import-sort'
 
 export default tseslint.config(
   { ignores: ['dist'] },
@@ -21,10 +22,13 @@ export default tseslint.config(
     plugins: {
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
+      'simple-import-sort': simpleImportSort,
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
       'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+      'simple-import-sort/imports': 'error',
+      'simple-import-sort/exports': 'error',
     },
   }
 )
--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,9 @@
+import './style/index.scss'
+
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+
 import App from './App.tsx'
-import './style/index.scss'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
